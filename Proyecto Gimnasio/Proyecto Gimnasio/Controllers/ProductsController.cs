59cc90a578e5b35c54ca7bad3654800b528046a5
Anthony22291 <anthonbeltran98@gmail.com>
2025-12-07 19:27:11 -0400

    Errores de Productos User y SalePlanes

--- Proyecto Gimnasio/Proyecto Gimnasio/Controllers/ProductsController.cs
+++ Proyecto Gimnasio/Proyecto Gimnasio/Controllers/ProductsController.cs
@@ -60,6 +60,12 @@ namespace Proyecto_Gimnasio.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("IdProduct,NameProduct,Image,ImageFile,Stock,Price,ExpirationDate,IdCategory")] Product product)
         {
+            // Validación de fecha
+            if (product.ExpirationDate.Date < DateTime.Today)
+            {
+                ModelState.AddModelError("ExpirationDate", "Expiration date cannot be before today");
+            }
+
             try
             {
                 if (product.ImageFile != null && product.ImageFile.Length > 0)
@@ -121,6 +127,13 @@ namespace Proyecto_Gimnasio.Controllers
             {
                 return NotFound();
             }
+
+            // Validación de fecha
+            if (product.ExpirationDate.Date < DateTime.Today)
+            {
+                ModelState.AddModelError("ExpirationDate", "Expiration date cannot be before today");
+            }
+
             if (product.ImageFile != null && product.ImageFile.Length > 0)
             {
                 var uploadsFolder = Path.Combine(_env.WebRootPath, "Images");
--- Proyecto Gimnasio/Proyecto Gimnasio/Models/Category.cs
+++ Proyecto Gimnasio/Proyecto Gimnasio/Models/Category.cs
@@ -10,8 +10,9 @@ namespace Proyecto_Gimnasio.Models
 		[Required(ErrorMessage = "Name is required")]
 		[StringLength(50, ErrorMessage = "{0} must be: minimum {2} and maximum {1}", MinimumLength = 5)]
 		[Display(Name = "NAME  CATEGORY")]
-		[RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Name must contain only letters")]
-		public string NameCategory { get; set; }
+        [RegularExpression(@"^[a-zA-Z0-9]+(?:\s+[a-zA-Z0-9]+)*$", ErrorMessage = "Name must contain only letters, numbers and spaces (no leading/trailing spaces)")]
+
+        public string NameCategory { get; set; }
 
 		// Una categoría tiene muchos productos
 		public ICollection<Product>? Products { get; set; }
