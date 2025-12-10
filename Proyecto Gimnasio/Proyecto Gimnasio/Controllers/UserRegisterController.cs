e0fcc3b6ac3d297f722c84dd19bad4144d34f167
Leovch54 <leandroveizagacrespo@gmail.com>
2025-12-09 21:04:10 -0400

    actualizado

--- Proyecto Gimnasio/Proyecto Gimnasio/Controllers/UserRegisterController.cs
+++ Proyecto Gimnasio/Proyecto Gimnasio/Controllers/UserRegisterController.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Proyecto_Gimnasio.Data;
 using Proyecto_Gimnasio.Models;
+using BCrypt.Net;
 
 namespace Proyecto_Gimnasio.Controllers
 {
@@ -22,162 +21,84 @@ namespace Proyecto_Gimnasio.Controllers
         // GET: UserRegister
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Users.ToListAsync());
+            return View(await _context.Users.Include(u => u.Person).ToListAsync());
         }
 
-        // GET: UserRegister/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var user = await _context.Users
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            return View(user);
-        }
-
-        // POST: UserRegister/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        // IMPORTANTE: Agregamos "Person" al Bind
-
+        // GET: UserRegister/Create
         public IActionResult Create()
         {
-            // Opcional: Si quieres inicializar la persona vacía para evitar errores null en la vista
-            // return View(new User { Person = new Person() }); 
             return View();
         }
 
-        public async Task<IActionResult> Create([Bind("Id,Email,Person")] User user)
-        {
-            // --- LÓGICA DE AUTO-COMPLETADO ---
-
-            // 1. Asignar valores por defecto para USER
-            user.Rol = "User";
-            user.primarySession = true;
-            // Contraseña temporal (porque es required)
-            user.Password = "Temporal123$";
-
-            // 2. Limpiar errores del ModelState de los campos que ocultamos
-            ModelState.Remove("Rol");
-            ModelState.Remove("Password");
-            ModelState.Remove("primarySession");
-
-            // NOTA: No necesitamos limpiar Name/Cnit/Gender porque ahora sí están en el formulario.
-
-            // 3. Validación de Email duplicado
-            if (await _context.Users.AnyAsync(u => u.Email == user.Email))
-            {
-                ModelState.AddModelError("Email", "Este correo ya está registrado.");
-            }
-
-            if (ModelState.IsValid)
-            {
-                // EF Core es inteligente: al guardar User, guardará también Person
-                // y asignará automáticamente el UserId a la persona.
-                _context.Add(user);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-
-            // Si falla, retornamos a la vista para mostrar errores
-            return View(user);
-        }
-
-        // GET: UserRegister/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var user = await _context.Users.FindAsync(id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-            return View(user);
-        }
-
-        // POST: UserRegister/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // POST: UserRegister/Create
+        // POST: UserRegister/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Email,Password,primarySession,Rol")] User user)
+        public async Task<IActionResult> Create([Bind("Id,Email,Password,Person")] User user)
         {
-            if (id != user.Id)
+            try
             {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
+                // 1. VALIDAR DUPLICADOS
+                if (await _context.Users.AnyAsync(u => u.Email == user.Email))
                 {
-                    _context.Update(user);
+                    ModelState.AddModelError("Email", "Este email ya está registrado");
+                    return View(user);
+                }
+
+                if (user.Person != null)
+                {
+                    if (await _context.Persons.AnyAsync(p => p.Cnit == user.Person.Cnit))
+                    {
+                        ModelState.AddModelError("Person.Cnit", "Este CNIT ya está registrado");
+                        return View(user);
+                    }
+                }
+
+                // 2. CREAR Y GUARDAR EL USUARIO
+                var newUser = new User
+                {
+                    Email = user.Email,
+                    Password = BCrypt.Net.BCrypt.HashPassword(user.Password),
+                    Rol = "Customer",
+                    primarySession = true
+                };
+
+                _context.Users.Add(newUser);
+                await _context.SaveChangesAsync();
+
+                // 3. CREAR Y GUARDAR LA PERSONA
+                if (user.Person != null)
+                {
+                    var newPerson = new Person
+                    {
+                        Name = user.Person.Name,
+                        LasName = user.Person.LasName,
+                        SecondLastName = user.Person.SecondLastName,
+                        DateBirthay = user.Person.DateBirthay,
+                        Cnit = user.Person.Cnit,
+                        Gender = user.Person.Gender,
+                        UserId = newUser.Id,
+                        RegisterDate = DateTime.Now,
+                        LastDate = DateTime.Now,
+                        Status = true
+                    };
+
+                    _context.Persons.Add(newPerson);
                     await _context.SaveChangesAsync();
                 }
-                catch (DbUpdateConcurrencyException)
+
+                // --- CAMBIO AQUÍ: REDIRIGIR AL HOME ---
+                return RedirectToAction("User", "Home");
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = "Error: " + ex.Message;
+                if (ex.InnerException != null)
                 {
-                    if (!UserExists(user.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
+                    ViewBag.Error += " | Inner: " + ex.InnerException.Message;
                 }
-                return RedirectToAction(nameof(Index));
+                return View(user);
             }
-            return View(user);
-        }
-
-        // GET: UserRegister/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var user = await _context.Users
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            return View(user);
-        }
-
-        // POST: UserRegister/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var user = await _context.Users.FindAsync(id);
-            if (user != null)
-            {
-                _context.Users.Remove(user);
-            }
-
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool UserExists(int id)
-        {
-            return _context.Users.Any(e => e.Id == id);
         }
     }
 }
